51862725adc02f1ef93d2b02ee5457d36688743d
yulduz <yulduz83@gmail.com>
2021-02-07 11:15:22 -0600

    copy and write functions are now converted into promises in utils

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const generatePage = require("./src/page-template");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 const mockData = {
   name: "Lernantino",
@@ -193,22 +193,21 @@ Add a New Project
     });
 };
 
-// promptUser()
-//   .then(promptProject)
-//   .then((portfolioData) => {
-//     const pageHTML = generatePage(portfolioData);
-
-//     fs.writeFile('./index.html', pageHTML, err => {
-//       if (err) throw new Error(err);
-
-//       console.log('Page created! Check out index.html in this directory to see it!');
-//     });
-//   });
-
-const pageHTML = generatePage(mockData);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
-
-  console.log('Page created! Check out index.html in this directory to see it!');
-});
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
